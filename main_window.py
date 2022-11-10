ed2e6e6b68a5dbf16bfd1c8047f773cab97e4ac1
MagGoldi <stivenH0king@yandex.ru>
2022-11-10 10:04:06 +0400

    import

--- main_window.py
+++ main_window.py
@@ -1,11 +1,13 @@
 import sys
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget
+from typing import Self
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-
+    folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
     w = QWidget()
     w.resize(250, 150)
     w.move(300, 300)
