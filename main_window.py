ceecf4b1e57ea28f1291e290939cae3915cee3e6
MagGoldi <StivenH0king@yandex.ru>
2022-11-12 15:40:18 +0400

    appdate

--- main_window.py
+++ main_window.py
@@ -1,8 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QFileDialog
+import PyQt5
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox
+from PyQt5.QtGui import QPixmap, QFont, QIcon
+from PyQt5.QtCore import Qt
 
-
-class Example(QWidget):
+class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -12,9 +15,114 @@ class Example(QWidget):
 
     def initUI(self):
 
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Message box')
-        self.show()
+        super(Window, self).__init__()
+
+        self.setGeometry(300, 250, 1007, 925)
+        self.setFixedSize(1007, 925)
+        self.setWindowTitle("Данные о")
+        self.background = QLabel(self)
+        self.fire = QLabel(self)
+       # self.background = QPixmap('cat_with_lighting.jpeg')
+        self.background.setGeometry(0, 0, 1007, 925)
+        self.background.setPixmap(QPixmap('cat_with_lighting.jpeg'))
+
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Создание и выбор аннотаций")
+        self.main_text.move(725, 10)
+        self.main_text.adjustSize()
+
+        self.folderpath_dataset = ""
+        while self.folderpath_dataset == "":
+            self.folderpath_dataset = QtWidgets.QFileDialog.getExistingDirectory(
+                self, "Please select folder of dataset"
+            )
+
+        self.botton1 = QtWidgets.QPushButton(self)
+        self.botton1.setText("Разделение на года")
+        self.botton1.setFixedWidth(300)
+        self.botton1.move(670, 30)
+        self.botton1.clicked.connect(self.year)
+
+        self.botton2 = QtWidgets.QPushButton(self)
+        self.botton2.setText("Разделение на недели")
+        self.botton2.setFixedWidth(300)
+        self.botton2.move(670, 60)
+        self.botton2.clicked.connect(self.week)
+
+        self.botton3 = QtWidgets.QPushButton(self)
+        self.botton3.setText("Разделение на даты и данные")
+        self.botton3.setFixedWidth(300)
+        self.botton3.move(670, 90)
+        self.botton3.clicked.connect(self.data_date)
+
+    def hidden_text(self):
+        self.text.setText("Введите дату дд/мм/гггг")
+        self.text.move(735, 120)
+        self.text.adjustSize()
+
+        self.fire.setGeometry(0, 0, 686, 520)
+        self.fire.setPixmap(QPixmap('pngwing.com.png'))
+        self.fire.hide()   
+
+        # self.calendar.setText("hjh")
+        self.dateEdit.show()
+        self.dateEdit.move(760, 136)
+        # self.dateEdit.setFixedWidth(200)
+
+        self.weather_text.setText("Данные")
+        self.weather_text.move(135, 310)
+        # self.weather_text.setFixedWidth(400)
+        self.weather_text.adjustSize()
+        self.weather_text.setStyleSheet("color: rgb(0, 0, 0); font-size: 18px;")
+        self.weather_text.hide()
+
+        self.btn_weather.show()
+        self.btn_weather.setText("Получить данные")
+        self.btn_weather.setFixedWidth(200)
+        self.btn_weather.move(720, 170)
+        self.btn_weather.clicked.connect(self.data_of_weather)
+
+    def data_date(self) -> None:
+        separation_by_date_and_data.main()
+        self.flag = 1
+        self.hidden_text()
+
+    def week(self) -> None:
+        separation_on_weeks.main()
+        self.flag = 2
+        self.hidden_text()
+
+    def year(self) -> None:
+        separation_on_year.main()
+        self.flag = 3
+        self.hidden_text()
+
+    def data_of_weather(self) -> None:
+        text = ""
+        self.fire.show()
+        self.weather_text.show()
+        if self.flag == 1:
+            text = search_with_date.search_in_data_and_date(
+                search_with_date.day(self.dateEdit.dateTime().toString("dd-MM-yyyy"))
+            )
+        else:
+            if self.flag == 2:
+                text = search_with_date.search_in_week_fast(
+                    search_with_date.day(
+                        self.dateEdit.dateTime().toString("dd-MM-yyyy")
+                    )
+                )
+            else:
+                text = search_with_date.search_in_year(
+                    search_with_date.day(
+                        self.dateEdit.dateTime().toString("dd-MM-yyyy")
+                    )
+                )
+        self.weather_text.setText(text)
+       # self.weather_text.move(515, 230)
+        self.weather_text.setFixedWidth(400)
+
 
 
     def closeEvent(self, event):
@@ -28,15 +136,18 @@ class Example(QWidget):
         else:
             event.ignore()
 
-    def get_directory(self):
+    """def get_directory(self):
         way = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")  
         print(way)    
-        return way    
+        return way"""  
 
 
-if __name__ == '__main__':
-
+def application():
     app = QApplication(sys.argv)
-    ex = Example()
-    print(ex.get_directory())
+    w = Window()
+    w.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
